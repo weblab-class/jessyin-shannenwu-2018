05169d7eb08d1f74bf911286b03c46a194034520
Jessica Yin <jyin210@gmail.com>
2018-01-23 14:25:24 -0500

    posts unique to users show up on profile page now

--- html-version/public/js/profile.js
+++ html-version/public/js/profile.js
@@ -1,39 +1,76 @@
 function main() {
-  const profileId = window.location.search.substring(1);
-  get('/api/user', {'_id' : profileId}, function(profileUser){
-      renderUserData(profileUser);
-  });
-    get('/api/whoami', {}, function(user) {
-    renderNavbar(user);
-  });
+    const profileId = window.location.search.substring(1);
+    get('/api/user', {
+        '_id': profileId
+    }, function (profileUser) {
+        renderUserData(profileUser);
+        console.log('hi');
+        get('/api/posts', {
+            creator_id: profileId
+        }, function (postsArr) {
+            console.log('hi');
+            for (let i = 0; i < postsArr.length; i++) {
+                console.log(postsArr[i].creator_id);
+                if (profileUser._id == postsArr[i].creator_id) {
+                    renderUserPosts(postsArr[i]);
+                }
+            }
+        });
+    });
+
+    get('/api/whoami', {}, function (user) {
+        renderNavbar(user);
+    });
 
 
 }
 
+function renderUserPosts(postJSON) {
+
+    const postContainer = document.getElementById('gallery-container');
+    const colDiv = document.createElement('div');
+    colDiv.className = 'col-sm';
+    postContainer.appendChild(colDiv);
+
+    const card = document.createElement('div');
+    card.setAttribute('id', postJSON._id);
+    card.className = 'card';
+    colDiv.appendChild(card);
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    card.appendChild(cardBody);
+
+    const contentSpan = document.createElement('p');
+    contentSpan.className = 'post-content card-text';
+    contentSpan.innerHTML = postJSON.content;
+    cardBody.appendChild(contentSpan);
+
+    const cardFooter = document.createElement('div');
+    cardFooter.className = 'card-footer';
+    card.appendChild(cardFooter);
+
+    const creatorSpan = document.createElement('a');
+    creatorSpan.className = 'post-creator card-title';
+    creatorSpan.innerHTML = postJSON.creator_name;
+    creatorSpan.setAttribute('href', '/u/profile?' + postJSON.creator_id)
+    cardFooter.appendChild(creatorSpan);
+
+    return card;
+}
+
 function renderUserData(user) {
-	// rendering name
-	const nameContainer = document.getElementById('name-container');
-	const nameHeader = document.createElement('h1');
-	nameHeader.innerHTML = user.name;
-	nameContainer.appendChild(nameHeader);
+    // rendering name
+    const nameContainer = document.getElementById('name-container');
+    const nameHeader = document.createElement('h1');
+    nameHeader.innerHTML = user.name;
+    nameContainer.appendChild(nameHeader);
 
-	// rendering profile image
-	const profileImage = document.getElementById('profile-image');
-	profileImage.style = 'background-image:url(https://i.pinimg.com/736x/98/e0/7d/98e07decc7c1ca58236995de3567e46a--cat-shirts-kitties-cutest.jpg)';
+    // rendering profile image
+    const profileImage = document.getElementById('profile-picture');
+    profileImage.style = 'background:url(/static/css/propic.jpg) 50% 50% no-repeat; background-size:cover;';
 
-	// rendering latest post
-	const latestPostCard = document.getElementById('latest-post-card');
 
-  const creatorSpan = document.createElement('a');
-  creatorSpan.className = 'story-creator card-title';
-  creatorSpan.innerHTML = user.name;
-  creatorSpan.setAttribute('href', '/u/profile?'+user._id);
-  latestPostCard.appendChild(creatorSpan);
-
-	const latestPost = document.createElement('p');
-	latestPost.className = 'story-content card-text';
-  latestPost.innerHTML = user.last_post;
-  latestPostCard.appendChild(latestPost);
 }
 
 main();
